baa2d14682b81443edb3cce8f8cead4d9a7199c7
Akhil Puri <126008014+cookies042004@users.noreply.github.com>
2026-01-16 11:38:46 +0530

    Delete src/week1 directory

--- src/week1/programming_elements/practice_problem/Welcome.java
+++
@@ -1,7 +0,0 @@
-package week1.programming_elements.practice_problem;
-
-public class Welcome {
-    public static void main(String[] args){
-        System.out.println("Welcome to Bridgelabz!");
-    }
-}
